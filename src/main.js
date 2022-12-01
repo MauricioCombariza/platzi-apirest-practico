be3b09ae84003737a9409be0c9a3819ea64d6d7c
MauricioCombariza <mcombarizav@gmail.com>
2022-12-01 05:45:59 -0500

    add api Rest

--- src/main.js
+++ src/main.js
@@ -1,1 +1,59 @@
-console.log(API_KEY);
+// import axios, { isCancel, AxiosError } from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  headers: {
+    "Content-Type": "application/json;charset=utf-8",
+  },
+  params: {
+    api_key: API_KEY,
+  },
+});
+
+async function getTrendingMoviesPreview() {
+  const { data } = await api("trending/movie/day?");
+
+  const trendingPreviewMoviesContainer = document.querySelector(
+    "#trendingPreview .trendingPreview-movieList"
+  );
+  const movies = data.results;
+  movies.forEach((movie) => {
+    const movieContainer = document.createElement("div");
+    movieContainer.classList.add("movie-container");
+
+    const movieImg = document.createElement("img");
+    movieImg.setAttribute(
+      "src",
+      "https://image.tmdb.org/t/p/w300/" + movie.poster_path
+    );
+    movieImg.classList.add("movie-img");
+    movieImg.setAttribute("alt", movie.title);
+
+    movieContainer.appendChild(movieImg);
+    trendingPreviewMoviesContainer.appendChild(movieContainer);
+  });
+}
+
+async function getCategoriesPreview() {
+  const { data } = await api("genre/movie/list?");
+
+  const previewcategoriesContainer = document.querySelector(
+    "#categoriesPreview .categoriesPreview-list"
+  );
+  const categories = data.genres;
+  categories.forEach((category) => {
+    const categoryContainer = document.createElement("div");
+    categoryContainer.classList.add("category-container");
+
+    const categoryTitle = document.createElement("h3");
+    categoryTitle.classList.add("category-title");
+    categoryTitle.setAttribute("id", "id" + category.id);
+    const categoryTitleText = document.createTextNode(category.name);
+    categoryTitle.appendChild(categoryTitleText);
+    categoryContainer.appendChild(categoryTitle);
+    previewcategoriesContainer.appendChild(categoryContainer);
+  });
+}
+
+getTrendingMoviesPreview();
+getCategoriesPreview();
